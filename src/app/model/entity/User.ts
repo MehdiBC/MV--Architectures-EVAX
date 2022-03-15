5997f6ca173f38928df6b7ff3a24834588eb3c34
Mehdi <mehdi.b.chikha@gmail.com>
2022-03-15 23:46:18 +0100

    pass-covid

--- src/app/model/entity/User.ts
+++ src/app/model/entity/User.ts
@@ -1,10 +1,12 @@
+import {Vaccine} from "./Vaccine";
+
 export class User{
   constructor(
     public numInscription: string,
     public firstName: string,
     public lastName: string,
     public cin: string,
-    public birthdate: Date,
+    public birthdate: string,
     public vaccines: Vaccine[],
   ) {
   }
--- src/app/view-model/pass-covid/pass-covid.component.ts
+++ src/app/view-model/pass-covid/pass-covid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../model/entity/User";
+import {UserDao} from "../../model/dao/UserDao";
 
 @Component({
   selector: 'app-pass-covid',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../view/pass-covid/pass-covid.component.css']
 })
 export class PassCovidComponent implements OnInit {
+  user: User | undefined;
+  numInscription: string = '';
 
-  constructor() { }
+  constructor() {
+    this.user = undefined;
+  }
 
   ngOnInit(): void {
   }
 
+  updateUser() {
+    this.user = UserDao.getUserByNumInscription(this.numInscription);
+  }
 }
